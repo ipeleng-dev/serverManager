f55990be1c6842772b7949742a0061c4bc2e71ba
ipeleng-dev <ipelengsilas@yahoo.com>
2023-07-28 18:32:32 +0200

    project upload

---
+++ src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.server.service.implementation;
+
+import com.example.server.model.Server;
+import com.example.server.service.ServerService;
+
+import java.util.Collection;
+
+public class ServerServiceImpl implements ServerService {
+
+    @Override
+    public Server create(Server server) {
+        return null;
+    }
+
+    @Override
+    public Server ping(String ipAddress) {
+        return null;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public Server get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+}
