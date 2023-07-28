2eb39fa4c23d398cc2371ff786e2b3f4fffe9324
ipeleng-dev <ipelengsilas@yahoo.com>
2023-07-28 18:54:29 +0200

    project upload

--- src/main/java/com/example/server/service/ServerService.java
+++ src/main/java/com/example/server/service/ServerService.java
@@ -2,11 +2,12 @@ package com.example.server.service;
 
 import com.example.server.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.server.service.implementation;
 
+
 import com.example.server.model.Server;
 import com.example.server.repo.ServerRepo;
 import com.example.server.service.ServerService;
@@ -8,7 +9,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+import static com.example.server.enumeration.Status.SERVER_DOWN;
+import static com.example.server.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
+import static org.springframework.data.domain.PageRequest.of;
 
 @RequiredArgsConstructor
 @Service
@@ -20,34 +28,44 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Server create(Server server) {
-        log.info("Saving new Server: {}", server.getName());
+        log.info("Saving new server: {}", server.getName());
         server.setImageUrl(setServerImageUrl());
-        return null;
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(of(0,limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}",id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by id: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
     }
 
     //methods
