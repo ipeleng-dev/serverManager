d27c31e0bad6332d1ac37ceb0d2bf725e4f300dc
ipeleng-dev <ipelengsilas@yahoo.com>
2023-07-28 16:56:35 +0200

    project upload

---
+++ src/main/java/com/example/server/enumeration/Status.java
@@ -0,0 +1,16 @@
+package com.example.server.enumeration;
+
+public enum Status {
+    SERVER_UP("SERVER_UP"),
+    SERVER_DOWN("SERVER_DOWN");
+
+    private String status;
+
+    Status(Status status){
+        this.status = status;
+    }
+
+    public String getStatus(){
+        return this.status;
+    }
+}
--- src/main/java/com/example/server/model/Server.java
+++ src/main/java/com/example/server/model/Server.java
@@ -1,6 +1,13 @@
 package com.example.server.model;
 
+import com.example.server.enumeration.Status;
+
 public class Server {
     private Long id;
     private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
 }
