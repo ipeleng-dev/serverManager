2844985f95088b3b96a183e3adc5ba947ad46353
ipeleng-dev <ipelengsilas@yahoo.com>
2023-07-28 18:29:42 +0200

    project upload

--- src/main/java/com/example/server/model/Server.java
+++ src/main/java/com/example/server/model/Server.java
@@ -1,9 +1,27 @@
 package com.example.server.model;
 
 import com.example.server.enumeration.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import static jakarta.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
     private String ipAddress;
     private String name;
     private String memory;
